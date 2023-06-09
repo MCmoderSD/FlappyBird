672d6a1a348bd215c984a86a58dccc949403e485
mcmodersd <mcmodersd@web.de>
2023-06-09 20:24:51 +0200

    ToDo's added pre logic

--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import javax.swing.text.NumberFormatter;
 import java.awt.*;
+import java.io.File;
+import java.util.Objects;
 
 public class UI extends JFrame {
     public static UI instance;
@@ -13,6 +15,7 @@ public class UI extends JFrame {
     private JTextField playerName;
     private JLabel score;
     private JSpinner spinnerTPS;
+    private Timer refreshLeaderBoard;
     private int TPS = 100;
     private boolean newGame = true, isUploaded = true;
 
@@ -21,10 +24,11 @@ public class UI extends JFrame {
         Background = backgroundImage;
         instance = this;
 
-        initFrame(width, height, title, icon, resizable, scoredPoints, Tickrate);
+        initFrame(width, height, title, icon, resizable, backgroundImage, args, scoredPoints, Tickrate);
 
         score.setVisible(false);
-        playerName.setVisible(false);
+        playerName.setVisible(true);
+        playerName.setText("Test"); // ToDo Fix Username Length Bug
         leaderBoard.setVisible(false);
         soundCheckBox.setSelected(true);
 
@@ -43,9 +47,8 @@ public class UI extends JFrame {
         dispose();
     }
 
-    public void initFrame(int width, int height, String title, String icon, boolean resizable, int points, int Tickrate) {
-        if (Tickrate <= TPS)
-            TPS = Tickrate;
+    public void initFrame(int width, int height, String title, String icon, boolean resizable, String backgroundImage, String[] args, int points, int Tickrate) {
+        if (Tickrate <= TPS) TPS = Tickrate;
 
         add(UI);
         setTitle(title);
@@ -56,6 +59,7 @@ public class UI extends JFrame {
         setIconImage((Methods.instance.reader(icon)));
         Movement.instance.backgroundResetX = 0;
         UI.repaint();
+        initLeaderBoard(width, height, title, icon, resizable, backgroundImage, Tickrate, args, points);
 
         if (points >= 0) {
             isUploaded = false;
@@ -73,6 +77,19 @@ public class UI extends JFrame {
         bStart.setToolTipText("Nochmal Spielen");
         isUploaded = true;
         newGame = true;
+        if (!(playerName.getText().length() > 0)) {
+            if (playerName.getText().length() >= 32) {
+                if (checkUserName(playerName.getText())) {
+                    writeLeaderBoard();
+                } else {
+                    JOptionPane.showMessageDialog(null, "Der Username ist nicht erlaubt!", "Fehler", JOptionPane.ERROR_MESSAGE);
+                    upload();
+                }
+            } else {
+                JOptionPane.showMessageDialog(null, "Der Username ist zu lang!", "Fehler", JOptionPane.ERROR_MESSAGE);
+                upload();
+            }
+        }
     }
 
     private boolean getSound() {
@@ -107,13 +124,48 @@ public class UI extends JFrame {
         ((NumberFormatter) txt.getFormatter()).setAllowsInvalid(false); // Verhindert ungültige Eingaben
     }
 
-// --Commented out by Inspection START (31.05.2023 04:08):
-//    private void initLeaderBoard() {
-//        setVisible(true);
-//        Timer refreshLeaderBoard = new Timer(1000, e -> {
-//
-//        });
-//        refreshLeaderBoard.start();
-//    }
-// --Commented out by Inspection STOP (31.05.2023 04:08)
+    @SuppressWarnings("unused")
+    private void initLeaderBoard(int width, int height, String title, String icon, boolean resizable, String backgroundImage, int Tickrate, String[] args, int points) {
+        leaderBoard.setVisible(true);
+        // ToDo Daten vom SQL Server holen und in die Tabelle einfügen
+        refreshLeaderBoard = new Timer(1000, e -> {
+            if (checkSQLConnection()) {
+                // ToDo Daten vom SQL Server holen und in die Tabelle einfügen
+            } else {
+                JOptionPane.showMessageDialog(null, "Es konnte keine Verbindung zum SQL Server hergestellt werden!", "Fehler", JOptionPane.ERROR_MESSAGE);
+                handleNoSQLConnection(width, height, title, icon, resizable, backgroundImage, Tickrate, args, points);
+            }
+        });
+        refreshLeaderBoard.start();
+    }
+
+    private void handleNoSQLConnection(int width, int height, String title, String icon, boolean resizable, String backgroundImage, int Tickrate, String[] args, int points) {
+        JOptionPane.showMessageDialog(null, "Es konnte keine Verbindung zum SQL Server hergestellt werden! \nVersuche es nochmal order gib kein Username ein", "Fehler", JOptionPane.ERROR_MESSAGE);
+        refreshLeaderBoard.stop();
+        new UI(width, height, title, icon, resizable, backgroundImage, Tickrate, args, points);
+    }
+
+    private void writeLeaderBoard() {
+        String userSQL = ""; // ToDo Usernamen vom SQL Server holen
+
+        int scoreSQL = 0; // ToDo Score vom SQL Server holen
+
+        // ToDo Daten in die Tabelle einfügen
+
+        if (Objects.equals(playerName.getText(), userSQL) && scoredPoints > scoreSQL) {
+            // ToDo Score vom SQL Server aktualisieren
+        }
+    }
+
+    @SuppressWarnings("unused")
+    private boolean checkUserName(String userName) {
+        File blockedTerms = new File("src/main/resources/blockedTerms.txt");
+        // ToDo userName in blockedTerms suchen
+        return true;
+    }
+
+    private boolean checkSQLConnection() {
+        // ToDo SQL Server Verbindung testen
+        return true;
+    }
 }
